9074f25fcc1fcfd4b3754acd6732e6126c1ce8f8
mina <takhminam@gmail.com>
2021-02-03 14:22:32 +0300

    adding spec files

---
+++ spec/programmers_jokes_spec.rb
@@ -0,0 +1,15 @@
+require 'programmers_jokes'
+
+describe 'Jokes' do
+let(:arr_url) { ['https://miro.medium.com/max/590/1*uG60w6w0Fw0SnXgv3j427Q.png'] }
+
+describe '#random_jokes' do
+    it'randomly select element from the array and return it to the user' do
+        expect(arr_url.sample).to eq(arr_url[0])
+    end
+    it 'return nil when there is no element in the array' do
+        arr = []
+        expect(arr.sample).to eq(nil)
+    end
+end
+end
---
+++ spec/programmers_sarcasm_spec.rb
@@ -0,0 +1,15 @@
+require 'programmers_sarcasm'
+
+describe 'Sarcasm' do
+let(:arr_url) { ['https://scontent.fesb4-2.fna.fbcdn.net/v/t1.0-9/96066463_237835897651629_218018501359566848_n.jpg?_nc_cat=102&ccb=2&_nc_sid=730e14&_nc_ohc=bYiboZ9tMDkAX9TYMAW&_nc_ht=scontent.fesb4-2.fna&oh=644efcb5e8734962296410076810a1ca&oe=603F0903'] }
+
+describe '#random_sarcasm' do
+    it'select an element from the array and return it to the user' do
+        expect(arr_url.sample).to eq(arr_url[0])
+    end
+    it 'return nil when there is no element in the array' do
+        arr = []
+        expect(arr.sample).to eq(nil)
+    end
+end
+end
