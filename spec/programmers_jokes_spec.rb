0b8ae07c0a5390a8b713ef9b0c3af53c3e2276b3
mina <takhminam@gmail.com>
2021-02-05 20:48:03 +0300

    Add negative cases to .spec files

--- spec/programmers_jokes_spec.rb
+++ spec/programmers_jokes_spec.rb
@@ -7,8 +7,9 @@ describe 'Jokes' do
     it 'randomly select element from the array and return it to the user' do
       expect(arr_url.sample).to eq(arr_url[0])
     end
-    it 'It does not return element in a sequential order' do
-      expect(arr_url.sample).not_to eq(arr_url)
+    it 'It does not return elements in a sequential order' do
+      arr = [22, 2, 100, 16]
+      expect(arr.sample(2)).not_to eq(arr[22, 2])
     end
     it 'return nil when there is no element in the array' do
       arr = []
